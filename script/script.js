d9c50f2fb75cac25c26b0ccbbf3e2b46512d36d8
ajaltdr <ajaltandukar@gmail.com>
2022-03-21 21:27:30 +0545

    Added paragraphs and javascript

---
+++ script/script.js
@@ -0,0 +1,81 @@
+// Scroll to the clicked section
+
+const about = document.getElementById("about");
+const aboutfooter = document.getElementById("about-footer");
+const about_section = document.getElementById("about-section");
+
+about.addEventListener('click', (event) =>  {
+    about_section.scrollIntoView(true);
+}
+);
+
+aboutfooter.addEventListener('click', (event) =>  {
+    about_section.scrollIntoView(true);
+}
+);
+
+
+const resume = document.getElementById("resume");
+const resumefooter = document.getElementById("resume-footer");
+const resume_section = document.getElementById("resume-section");
+
+resume.addEventListener('click', (event) =>  {
+    resume_section.scrollIntoView(true);
+}
+);
+
+resumefooter.addEventListener('click', (event) =>  {
+    resume_section.scrollIntoView(true);
+}
+);
+
+
+const certificate = document.getElementById("certificate");
+const certificatefooter = document.getElementById("certificate-footer");
+const certificate_section = document.getElementById("certificate-section");
+
+certificate.addEventListener('click', (event) =>  {
+    certificate_section.scrollIntoView(true);
+}
+);
+
+certificatefooter.addEventListener('click', (event) =>  {
+    certificate_section.scrollIntoView(true);
+}
+);
+
+
+const skills = document.getElementById("skills");
+const skillsfooter = document.getElementById("skills-footer");
+const skills_section = document.getElementById("skills-section");
+
+skills.addEventListener('click', (event) =>  {
+    skills_section.scrollIntoView(true);
+}
+);
+
+skillsfooter.addEventListener('click', (event) =>  {
+    skills_section.scrollIntoView(true);
+}
+);
+
+const contact = document.getElementById("contact");
+const contactfooter = document.getElementById("contact-footer");
+const contactBtn = document.getElementById("contact-btn")
+const contact_section = document.getElementById("contact-section");
+
+contact.addEventListener('click', (event) =>  {
+    contact_section.scrollIntoView(true);
+}
+);
+
+contactBtn.addEventListener('click', (event) =>  {
+    contact_section.scrollIntoView(true);
+}
+);
+
+contactfooter.addEventListener('click', (event) =>  {
+    contact_section.scrollIntoView(true);
+}
+);
+
